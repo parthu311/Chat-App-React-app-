f272eb552c2b4d3c9d04b6464479d300dbf3d0ca
parthu311 <parthradadiya433@gmail.com>
2024-02-20 12:37:15 +0530

    Nord Done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <>
+    <ChatEngine
+        height='100vh'
+        projectID="b35fdd0b-ef2a-4624-87ff-1b9c99f7e1d1"
+        userName="parthu311"
+        userSecret="12345678"
+    />
+   </>
+  );
+}
+
+export default App;
